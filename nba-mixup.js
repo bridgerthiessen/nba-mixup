4453ec99b6539fb7adcf307c02994901e3c7da16
Bridger Thiessen <bjthiessen@mun.ca>
2021-10-18 13:09:51 -0230

    added number to output

--- nba-mixup.js
+++ nba-mixup.js
@@ -1,13 +1,15 @@
 firstNames = ['Steph', 'Lebron', 'Kevin', 'Giannis', 'James'];
 lastNames = ['Curry', 'James', 'Durant', 'Antetenkounmpo', 'Harden'];
+number = ['30', '6', '7', '34', '13']
 
 var pickFirst = firstNames[Math.floor(Math.random()*firstNames.length)];
 var pickLast = lastNames[Math.floor(Math.random()*lastNames.length)];
+var pickNumber = number[Math.floor(Math.random()*number.length)];
 
 
 
 const returnMixedName = () => {
-  return `${pickFirst } ${pickLast}`
+  return `${pickFirst } ${pickLast}, ${pickNumber}`
 }
 
 console.log(returnMixedName())
